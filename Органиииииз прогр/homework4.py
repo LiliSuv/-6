b7ed1a54649e24f4ee92558e3fa4e2979c9b27e6
LiliSuv <faufy@yandex.ru>
2024-07-11 21:47:53 +0300

    Add files via upload

---
+++ Органиииииз прогр/homework4.py
@@ -0,0 +1,11 @@
+my_string=input("Введите первую строчку гимна РФ ") # ввести Россия священная наша держава
+print(my_string)
+print('Россия священная наша держава'==my_string) #проверка правильности введения данных
+print("Колличество символов введенного текста:",len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace('священная', 'великая'))
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
